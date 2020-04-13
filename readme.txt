f888ce03602b06e18c43f90a1511fcae83abc51a
Bill Schnoebelen <Bill@circuitcase.com>
2020-04-13 13:05:20 -0700

    first commit

---
+++ readme.txt
@@ -0,0 +1,1 @@
+https://www.youtube.com/watch?v=hEtZ040fsD8
---
+++ src/App.js
@@ -0,0 +1,143 @@
+import React, { useState, useEffect, useRef } from "react";
+import FlashcardList from "./FlashcardList";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [flashcards, setFlashcards] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  const categoryEl = useRef();
+  const amountEl = useRef();
+
+  useEffect(() => {
+    axios.get("https://opentdb.com/api_category.php").then((res) => {
+      setCategories(res.data.trivia_categories);
+      // console.log(res.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    axios.get("https://opentdb.com/api.php?amount=10").then((res) => {
+      setFlashcards(
+        res.data.results.map((questionItem, index) => {
+          const answer = decodeString(questionItem.correct_answer);
+          const options = [
+            ...questionItem.incorrect_answers.map((a) => decodeString(a)),
+            answer,
+            "Who are 3 people who've never been in my kitchen?",
+          ];
+          return {
+            id: `${index}-${Date.now()}`,
+            question: decodeString(questionItem.question),
+            answer:
+              answer + " or Who are 3 people who've never been in my kitchen?",
+            options: options.sort(() => Math.random() - 0.5),
+          };
+        })
+      );
+    });
+  }, []);
+
+  function decodeString(str) {
+    const textArea = document.createElement("textarea");
+    textArea.innerHTML = str;
+    return textArea.value;
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios
+      .get("https://opentdb.com/api.php", {
+        params: {
+          amount: amountEl.current.value,
+          category: categoryEl.current.value,
+        },
+      })
+      .then((res) => {
+        setFlashcards(
+          res.data.results.map((questionItem, index) => {
+            const answer = decodeString(questionItem.correct_answer);
+            const options = [
+              ...questionItem.incorrect_answers.map((a) => decodeString(a)),
+              answer,
+              "Who are 3 people who've never been in my kitchen?",
+            ];
+            return {
+              id: `${index}-${Date.now()}`,
+              question: decodeString(questionItem.question),
+              answer:
+                answer +
+                " or Who are 3 people who've never been in my kitchen?",
+              options: options.sort(() => Math.random() - 0.5),
+            };
+          })
+        );
+      });
+  }
+
+  return (
+    <>
+      <form className="header" onSubmit={handleSubmit}>
+        <div className="instructions">
+          <h3>Instructions:</h3>{" "}
+          <ul>
+            <li>1. Click on cards to see answers. </li>{" "}
+            <li>
+              2. Always Correct Answer: "Who are 3 people who've never been in
+              my kitchen?"
+            </li>
+          </ul>
+          <p>Good Luck,</p>
+          <h4>Jack Trebek</h4>
+        </div>
+        <div className="form-group">
+          <label htmlFor="category">Category</label>
+          <select id="category" ref={categoryEl}>
+            {categories.map((category) => {
+              return (
+                <option value={category.id} key={category.id}>
+                  {category.name}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">Number of Questions</label>
+          <input
+            type="number"
+            id="amount"
+            min="1"
+            steps="1"
+            defaultValue={10}
+            ref={amountEl}
+          />
+        </div>
+        <div className="form-group">
+          <button className="btn">Generate</button>
+        </div>
+      </form>
+      <div className="container">
+        <FlashcardList flashcards={flashcards} />
+      </div>
+    </>
+  );
+}
+
+// const SAMPLE_FLASHCARDS = [
+//   {
+//     id: 1,
+//     question: "What is 2 + 2?",
+//     answer: "84",
+//     options: ["2", "3", "84", "5"],
+//   },
+//   {
+//     id: 2,
+//     question: "Question 2?",
+//     answer: "Answer",
+//     options: ["Answer", "Wrong Answer 1", "Wrong Answer 2", "Wrong Answer 3"],
+//   },
+// ];
+
+export default App;
